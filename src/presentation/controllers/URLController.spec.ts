e0614b2d53f3a887da636b80b8f9eb3febfe7319
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-14 14:50:20 -0300

    feat: add Suffix Creator interface

--- src/presentation/controllers/URLController.spec.ts
+++ src/presentation/controllers/URLController.spec.ts
@@ -1,15 +1,34 @@
 import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, ok } from '../helpers/http-helper';
 import { URLController } from './URLController';
-
+import { SuffixCreator } from '../protocols/suffix-creator';
 describe('URLController', () => {
+  const makeSut = (): URLController => {
+    class SuffixCreatorStub implements SuffixCreator {
+      createSuffix() {
+        return 'SuFfIx';
+      }
+    }
+    return new URLController(new SuffixCreatorStub());
+  };
+
   it('Should return 400 if no URL is provided', () => {
-    const sut = new URLController();
+    const sut = makeSut();
     const httpRequest = {
       body: {},
     };
     const response = sut.handle(httpRequest);
     const expected = badRequest(new MissingParamError('URL'));
+    expect(response).toEqual(expected);
+  });
+
+  it('Should return 200 if URL is provided', () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: { url: 'http://g.com' },
+    };
+    const response = sut.handle(httpRequest);
+    const expected = ok({});
     expect(response).toEqual(expected);
   });
 });
--- src/presentation/controllers/URLController.ts
+++ src/presentation/controllers/URLController.ts
@@ -2,12 +2,21 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 import { Controller } from '../protocols/controller';
 import { badRequest, ok } from '../helpers/http-helper';
 import { MissingParamError } from '../errors/missing-param-error';
+import { SuffixCreator } from '../protocols/suffix-creator';
 
 export class URLController implements Controller {
+  private readonly suffixCreator: SuffixCreator;
+
+  constructor(suffixCreator: SuffixCreator) {
+    this.suffixCreator = suffixCreator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.url) {
       return badRequest(new MissingParamError('URL'));
     }
+    const suffix = this.suffixCreator.createSuffix();
+
     return ok({});
   }
 }
