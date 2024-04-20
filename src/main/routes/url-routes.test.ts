19ab1f13a6419527928ca91697d872d729729f76
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-20 12:23:22 -0300

    refactor: ensure mongo is connected on tests

--- src/main/routes/url-routes.test.ts
+++ src/main/routes/url-routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
 describe('URL Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+  beforeEach(async () => {
+    const urlCollection = MongoHelper.getCollection('urls');
+    await urlCollection.deleteMany({});
+  });
   it('Should return an URL on success ', async () => {
     await request(app)
       .post('/url')
