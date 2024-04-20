4e16261f4fcbd818d05ce688d8e938c6667e77e7
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-20 13:54:30 -0300

    refactor: connect on db before listening on app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,13 @@
 import app from './config/app';
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper';
 const port = 7000;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+MongoHelper.connect(process.env.MONGO_CONNECTION as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((e) => {
+    console.error(`[server]: Failed to connect to db ${e}`);
+  });
