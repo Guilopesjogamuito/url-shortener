b289ef06e797472ed23fe28c6e889e98cb80f73d
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-21 10:12:01 -0300

    test: ensure LoadURLController returns 400 when invalid suffix is provided

--- src/presentation/controllers/url/load-url-controller.spec.ts
+++ src/presentation/controllers/url/load-url-controller.spec.ts
@@ -1,11 +1,19 @@
-import { MissingParamError } from '../../errors';
+import { MissingParamError, InvalidParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
+import { SuffixValidator } from '../../protocols/suffix-validator';
 import { LoadURLController } from './load-url-controller';
 
 describe('LoadURLController', () => {
-  const makeSut = (): { sut: LoadURLController } => {
+  class SuffixValidatorStub implements SuffixValidator {
+    isValid() {
+      return true;
+    }
+  }
+  const makeSut = (): { sut: LoadURLController; suffixValidatorStub: SuffixValidatorStub } => {
+    const suffixValidatorStub = new SuffixValidatorStub();
     return {
-      sut: new LoadURLController(),
+      sut: new LoadURLController(suffixValidatorStub),
+      suffixValidatorStub,
     };
   };
 
@@ -16,6 +24,17 @@ describe('LoadURLController', () => {
     };
     const response = await sut.handle(httpRequest);
     const expected = badRequest(new MissingParamError('Suffix'));
+    expect(response).toEqual(expected);
+  });
+
+  it('Should return 400 if invalid suffix is provided', async () => {
+    const { sut, suffixValidatorStub } = makeSut();
+    jest.spyOn(suffixValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = {
+      params: { suffix: 'invalid_suffix' },
+    };
+    const expected = badRequest(new InvalidParamError('Suffix'));
+    const response = await sut.handle(httpRequest);
     expect(response).toEqual(expected);
   });
 });
