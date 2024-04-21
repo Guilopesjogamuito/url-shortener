dd3eec6fe89138a2b9e8c0b9dba351085a02aa5a
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-21 10:11:40 -0300

    feat: ensure LoadURLController returns 400 when invalid suffix is provided

--- src/presentation/controllers/url/load-url-controller.ts
+++ src/presentation/controllers/url/load-url-controller.ts
@@ -1,12 +1,22 @@
 import { Controller, HttpRequest, HttpResponse } from './url-protocols';
 import { badRequest, ok, serverError } from '../../helpers/http-helper';
-import { MissingParamError } from '../../errors';
+import { MissingParamError, InvalidParamError } from '../../errors';
+import { SuffixValidator } from '../../protocols/suffix-validator';
 
 export class LoadURLController implements Controller {
+  private readonly validator;
+
+  constructor(validator: SuffixValidator) {
+    this.validator = validator;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.params || !httpRequest.params.suffix) {
       return badRequest(new MissingParamError('Suffix'));
     }
+
+    const valid = this.validator.isValid(httpRequest.params.suffix);
+    if (!valid) return badRequest(new InvalidParamError('Suffix'));
 
     try {
       return ok({});
