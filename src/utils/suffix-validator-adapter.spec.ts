1dc1c3015bbb7a5ba94703f5d7ab87d2950d7d91
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-21 10:01:06 -0300

    test: ensure Suffix Validator return true on valid suffix

--- src/utils/suffix-validator-adapter.spec.ts
+++ src/utils/suffix-validator-adapter.spec.ts
@@ -7,4 +7,11 @@ describe('SuffixValidatorAdapter', () => {
     const valid = sut.isValid(invalidSuffix);
     expect(valid).toBe(false);
   });
+
+  it('Should validate suffix', () => {
+    const validSuffix = 'aBcDe';
+    const sut = new SuffixValidatorAdapter();
+    const valid = sut.isValid(validSuffix);
+    expect(valid).toBe(true);
+  });
 });
