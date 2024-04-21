120f01c3e8e89b3b3db19b7b9d2564656864e703
Guilherme Campello <guilhermelopes764@gmail.com>
2024-04-21 12:17:45 -0300

    test: add DbLoadURLBySuffix

---
+++ src/data/use-cases/load-url-by-suffix/db-load-url-by-suffix.spec.ts
@@ -0,0 +1,32 @@
+import { URLModel, LoadURLBySuffixRepository } from './db-load-url-by-suffix-protocols';
+import { DbLoadURLBySuffix } from './db-load-url-by-suffix';
+
+describe('DbLoadURLBySuffix Usecase', () => {
+  const makeRepository = (): LoadURLBySuffixRepository => {
+    class LoadURLBySuffixRepositoryStub implements LoadURLBySuffixRepository {
+      load(suffix: string): Promise<URLModel> {
+        const fakeURL = {
+          originalURL: 'test',
+          suffix: 'test',
+          createdAt: new Date(),
+          expiresAt: new Date(),
+        };
+        return new Promise((resolve) => resolve(fakeURL));
+      }
+    }
+    return new LoadURLBySuffixRepositoryStub();
+  };
+
+  const makeSut = () => {
+    const repository = makeRepository();
+    return { sut: new DbLoadURLBySuffix(repository), loadURLBySuffixRepositoryStub: repository };
+  };
+
+  it('Should call LoadURLBySuffixRepository with correct values', async () => {
+    const { sut, loadURLBySuffixRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(loadURLBySuffixRepositoryStub, 'load');
+
+    await sut.load('SUFFIX');
+    expect(addSpy).toHaveBeenCalledWith('SUFFIX');
+  });
+});
